a9e9b33c61d46835040fbd75fab4e9ea2eaa4203
Ramón <esedelos@hotmail.com>
2024-04-02 14:47:43 -0600

    Ya funciona la trancision del menu hamburgesa

--- Vista/js/vehiculosRegistrados.js
+++ Vista/js/vehiculosRegistrados.js
@@ -1,6 +1,16 @@
-const menuIcon = document.querySelector('.menuIcon');
-const sideNav = document.querySelector('.sideNav');
-
-menuIcon.addEventListener('click', () => {
-  sideNav.classList.toggle('show');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menuIcon');
+    const sideNav = document.querySelector('.navMenu');
+    const content = document.querySelector('.content');
+  
+    menuIcon.addEventListener('click', () => {
+      sideNav.classList.toggle('show');
+    });
+  
+    content.addEventListener('click', (event) => {
+      // Si el menú está abierto y el clic ocurre fuera del menú, ciérralo
+      if (sideNav.classList.contains('show') && !sideNav.contains(event.target) && event.target !== menuIcon) {
+        sideNav.classList.remove('show');
+      }
+    });
+  });
