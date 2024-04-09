307297a1f72e8b290486ac56da9eb5b19f563afa
Ramón <esedelos@hotmail.com>
2024-04-08 21:41:14 -0600

    Ajustes a la camara

--- Vista/js/registrarVehiculos.js
+++ Vista/js/registrarVehiculos.js
@@ -2,22 +2,20 @@ const cameraButton = document.getElementById('cameraButton');
 const cameraPreview = document.getElementById('cameraPreview');
 const cameraCanvas = document.getElementById('cameraCanvas');
 const captureButton = document.getElementById('captureButton');
-
 let cameraStream = null;
 
 // Función para acceder a la cámara
-function accessCamera() {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-      cameraStream = stream;
-      cameraPreview.srcObject = stream;
-      cameraPreview.style.display = 'block';
-      cameraButton.style.display = 'none';
-      captureButton.style.display = 'inline-block';
-    })
-    .catch(error => {
-      console.error('Error al acceder a la cámara:', error);
-    });
+async function accessCamera() {
+  try {
+    cameraStream = await navigator.mediaDevices.getUserMedia({ video: true });
+    cameraPreview.srcObject = cameraStream;
+    cameraPreview.style.display = 'block';
+    cameraButton.style.display = 'none';
+    captureButton.style.display = 'inline-block';
+  } catch (error) {
+    console.error('Error al acceder a la cámara:', error);
+    alert('No se pudo acceder a la cámara. Asegúrate de que has dado los permisos necesarios.');
+  }
 }
 
 // Función para tomar una foto
