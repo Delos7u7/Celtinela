80991e2d96796365ea258c7a6e0ba0befd521226
Ramón <esedelos@hotmail.com>
2024-04-08 22:58:26 -0600

    Agregado el boton para subir archivos

--- Vista/js/registrarVehiculos.js
+++ Vista/js/registrarVehiculos.js
@@ -1,36 +1,17 @@
-const cameraButton = document.getElementById('cameraButton');
-const cameraPreview = document.getElementById('cameraPreview');
-const cameraCanvas = document.getElementById('cameraCanvas');
-const captureButton = document.getElementById('captureButton');
-let cameraStream = null;
-
-// Función para acceder a la cámara
-async function accessCamera() {
-  try {
-    cameraStream = await navigator.mediaDevices.getUserMedia({ video: true });
-    cameraPreview.srcObject = cameraStream;
-    cameraPreview.style.display = 'block';
-    cameraButton.style.display = 'none';
-    captureButton.style.display = 'inline-block';
-  } catch (error) {
-    console.error('Error al acceder a la cámara:', error);
-    alert('No se pudo acceder a la cámara. Asegúrate de que has dado los permisos necesarios.');
+document.getElementById("fileUpload").addEventListener("change", function() {
+  // Verificar si se cargó correctamente una imagen
+  if (this.files && this.files[0]) {
+      // Aquí puedes realizar cualquier otra acción relacionada con el manejo de la imagen
+      // Por ejemplo, mostrar la imagen cargada en algún lugar de tu página
+      var reader = new FileReader();
+      reader.onload = function(e) {
+          document.getElementById("previewImage").src = e.target.result;
+      }
+      reader.readAsDataURL(this.files[0]);
+      
+      // Cambiar la clase del label al de la palomita
+      var labelElement = document.querySelector('.icon--container label');
+      labelElement.classList.remove('bi', 'bi-upload', 'iconCamera');
+      labelElement.classList.add('bi', 'bi-check', 'iconSuccess');
   }
-}
-
-// Función para tomar una foto
-function takePhoto() {
-  const canvas = cameraCanvas;
-  const context = canvas.getContext('2d');
-  context.drawImage(cameraPreview, 0, 0, canvas.width, canvas.height);
-  cameraPreview.style.display = 'none';
-  canvas.style.display = 'block';
-  captureButton.style.display = 'none';
-  // Aquí puedes guardar o cargar la imagen capturada
-}
-
-// Evento click en el botón de la cámara
-cameraButton.addEventListener('click', accessCamera);
-
-// Evento click en el botón de tomar foto
-captureButton.addEventListener('click', takePhoto);
+});
