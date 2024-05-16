02c8ee0cf014ac0dd53c87ec6254378e0ed41577
Ramón <esedelos@hotmail.com>
2024-05-15 21:31:08 -0600

    Parte de los dispositivos y vinculacion con equipoos terminada

--- Vista/js/vehiculo.js
+++ Vista/js/vehiculo.js
@@ -21,7 +21,6 @@ document.getElementById('serie').textContent = serie;
 
 function linkDevice() {
     const id_dispositivo = document.getElementById("numero_serie").value;
-
     const params = new URLSearchParams(window.location.search);
     const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'));
     
@@ -53,14 +52,87 @@ function linkDevice() {
         });
 }
 
+function getAlerts() {
+  const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'));
+  const url = `http://192.168.0.37:8080/getAlerts?id_vehiculo=${id_vehiculo}`;
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  fetch(url, requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error en la solicitud: ' + response.statusText);
+      } else {
+        return response.json();
+      }
+    })
+    .then(data => {
+      console.log('Respuesta del servidor (getVehicle):', data);
+      generarFilaTabla(data); // Llamamos a la función para generar una fila de la tabla
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
 
 
+function generarFilaTabla(alerta) {
+  const contenedorTabla = document.querySelector('.alert-table--body');
+  contenedorTabla.innerHTML = ''; // Limpiamos el contenido previo
+
+  if (!alerta) {
+    const sinAlertas = document.createElement('p');
+    sinAlertas.textContent = 'No hay alertas disponibles.';
+    contenedorTabla.appendChild(sinAlertas);
+  } else {
+    const fila = document.createElement('div');
+    fila.classList.add('alert-row');
+    fila.style.display = 'flex';
+    fila.style.justifyContent = 'space-evenly';
+    fila.style.width = '100%';
+
+    const noAlerta = document.createElement('p');
+    noAlerta.textContent = alerta.id_alerta;
+    noAlerta.classList.add('id-alerta');
+    noAlerta.style.width = '25%'; // Aplicamos el ancho del 5%
+    noAlerta.style.fontSize = '10px'; // Aplicamos el tamaño de fuente de 10px
+    fila.appendChild(noAlerta);
+
+    const latitud = document.createElement('p');
+    latitud.textContent = alerta.latitud;
+    latitud.classList.add('latitud');
+    latitud.style.width = '25%'; // Aplicamos el ancho del 20%
+    fila.appendChild(latitud);
+
+    const longitud = document.createElement('p');
+    longitud.textContent = alerta.longitud;
+    longitud.classList.add('longitud');
+    longitud.style.width = '25%'; // Aplicamos el ancho del 20%
+    fila.appendChild(longitud);
+
+    const fecha = document.createElement('p');
+    fecha.textContent = alerta.fecha;
+    fecha.classList.add('fecha');
+    fecha.style.width = '25%'; // Aplicamos el ancho del 13%
+    fila.appendChild(fecha);
+
+    contenedorTabla.appendChild(fila);
+  }
+}
 
 document.getElementById('modal-form').addEventListener('submit', function (event) {
     event.preventDefault();
     linkDevice();
   });
 
+
+  document.addEventListener('DOMContentLoaded', function() {
+    getAlerts();
+});
 
 
 
