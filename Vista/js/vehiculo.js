2db7ddb3657d3f01d0a0cc41473972e3ce9e00e4
Jose Ramon <esedelos@hotmail.com>
2024-05-17 22:34:44 -0600

    Cambios de ryan/moc/heber no completos

--- Vista/js/vehiculo.js
+++ Vista/js/vehiculo.js
@@ -1,6 +1,6 @@
 // Leer los parámetros de la URL
 const params = new URLSearchParams(window.location.search);
-const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'))
+const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'));
 const nombre = decodeURIComponent(params.get('alias_vehiculo'));
 const año = params.get('año');
 const marca = decodeURIComponent(params.get('marca'));
@@ -16,28 +16,21 @@ document.getElementById('modelo').textContent = modelo;
 document.getElementById('placas').textContent = placas;
 document.getElementById('serie').textContent = serie;
 
-
-
-
 function linkDevice() {
     const id_dispositivo = document.getElementById("numero_serie").value;
-    const params = new URLSearchParams(window.location.search);
-    const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'));
-    
+
     const data = {
         id_dispositivo: id_dispositivo,
         id_vehiculo: id_vehiculo
     };
+
     const requestOptions = {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
     };
-    console.log(data);
-    const url = `http://192.168.56.1:8080/linkDevice`;
-    fetch(url, requestOptions)
+
+    fetch(`http://192.168.1.29:8080/linkDevice`, requestOptions)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error al enviar los datos al backend');
@@ -46,7 +39,7 @@ function linkDevice() {
         })
         .then(data => {
             console.log('Datos recibidos desde el backend:', data);
-            location.reload();  
+            location.reload();
         })
         .catch(error => {
             console.error('Error:', error);
@@ -54,179 +47,154 @@ function linkDevice() {
 }
 
 function getAlerts() {
-  const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'));
-  const url = `http://192.168.56.1:8080/getAlerts?id_vehiculo=${id_vehiculo}`;
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+    const url = `http://192.168.1.29:8080/getAlerts?id_vehiculo=${id_vehiculo}`;
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    };
 
-  fetch(url, requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error en la solicitud: ' + response.statusText);
-      } else {
-        return response.json();
-      }
-    })
-    .then(data => {
-      console.log('Respuesta del servidor (getVehicle):', data);
-      generarFilasTabla(data); // Llamamos a la función para generar las filas de la tabla
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Respuesta del servidor (getVehicle):', data);
+            generarFilasTabla(data);
+
+            if (data.length > 0) {
+                // Actualiza el mapa con la ubicación más reciente
+                const alertaMasReciente = data[0];
+                updateMap(alertaMasReciente.latitud, alertaMasReciente.longitud);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-
 function generarFilasTabla(datos) {
-  const contenedorTabla = document.querySelector('.alert-table--body');
-  contenedorTabla.innerHTML = ''; // Limpiamos el contenido previo
+    const contenedorTabla = document.querySelector('.alert-table--body');
+    contenedorTabla.innerHTML = '';
 
-  if (datos.length === 0) {
-    const sinAlertas = document.createElement('p');
-    sinAlertas.textContent = 'No hay alertas disponibles.';
-    contenedorTabla.appendChild(sinAlertas);
-  } else {
-    datos.forEach(alerta => {
-      const fila = document.createElement('div');
-      fila.classList.add('alert-row');
-      fila.style.display = 'flex';
-      fila.style.justifyContent = 'space-evenly';
-      fila.style.width = '100%'; // Agregamos width: 100%
+    if (datos.length === 0) {
+        const sinAlertas = document.createElement('p');
+        sinAlertas.textContent = 'No hay alertas disponibles.';
+        contenedorTabla.appendChild(sinAlertas);
+    } else {
+        datos.forEach(alerta => {
+            const fila = document.createElement('div');
+            fila.classList.add('alert-row');
+            fila.style.display = 'flex';
+            fila.style.justifyContent = 'space-evenly';
+            fila.style.width = '100%';
 
-      const noAlerta = document.createElement('p');
-      noAlerta.textContent = alerta.id_alerta;
-      noAlerta.classList.add('id-alerta');
-      noAlerta.style.width = '25%';
-      noAlerta.style.fontSize = '10px';
-      fila.appendChild(noAlerta);
+            const campos = ['id_alerta', 'latitud', 'longitud', 'fecha'];
+            campos.forEach(campo => {
+                const celda = document.createElement('p');
+                celda.textContent = alerta[campo];
+                celda.classList.add(campo);
+                celda.style.width = '25%';
+                fila.appendChild(celda);
+            });
 
-      const latitud = document.createElement('p');
-      latitud.textContent = alerta.latitud;
-      latitud.classList.add('latitud');
-      latitud.style.width = '25%';
-      fila.appendChild(latitud);
+            fila.addEventListener('click', () => {
+                updateMap(alerta.latitud, alerta.longitud);
+            });
 
-      const longitud = document.createElement('p');
-      longitud.textContent = alerta.longitud;
-      longitud.classList.add('longitud');
-      longitud.style.width = '25%';
-      fila.appendChild(longitud);
+            contenedorTabla.appendChild(fila);
+        });
+    }
+}
 
-      const fecha = document.createElement('p');
-      fecha.textContent = alerta.fecha;
-      fecha.classList.add('fecha');
-      fecha.style.width = '25%';
-      fila.appendChild(fecha);
-
-      contenedorTabla.appendChild(fila);
-    });
-  }
+function updateMap(latitud, longitud) {
+    const mapa = document.querySelector('.mapa');
+    const newSrc = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBIiZxQFCekeM29UC4gQD503JK0kLDf3Eg&q=${latitud},${longitud}`;
+    mapa.src = newSrc;
 }
 
 function changeAlertState() {
-  const id_vehiculo = decodeURIComponent(params.get('id_vehiculo'));
-  const url = `http://192.168.56.1:8080/changeAlertState?id_vehiculo=${id_vehiculo}`;
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+    const url = `http://192.168.1.29:8080/changeAlertState?id_vehiculo=${id_vehiculo}`;
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    };
 
-  fetch(url, requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error en la solicitud: ' + response.statusText);
-      } else {
-        return response.json();
-      }
-    })
-    .then(data => {
-      console.log('Respuesta del servidor (changeAlertState):', data);
-      location.reload();  
-
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Respuesta del servidor (changeAlertState):', data);
+            location.reload();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 document.getElementById('modal-form').addEventListener('submit', function (event) {
     event.preventDefault();
     linkDevice();
-  });
+});
 
-
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     getAlerts();
 });
 
-var btnCorrecto = document.getElementById('correcto');
-
-// Añadir evento de clic al botón de correcto
-btnCorrecto.onclick = function() {
-  changeAlertState();
-}
-
-
-
-
+document.getElementById('correcto').onclick = function() {
+    changeAlertState();
+};
 
 const mostrarMasBtn = document.querySelector('.button-mostras-mas');
 const mostrarMasContainer = document.querySelector('.mostrar-mas');
 const clonMostrarMas = document.querySelector('.clon-mostrar-mas');
 
 mostrarMasBtn.addEventListener('click', () => {
-  mostrarMasContainer.classList.toggle('active');
+    mostrarMasContainer.classList.toggle('active');
 });
 
 clonMostrarMas.addEventListener('click', () => {
-  mostrarMasContainer.classList.remove('active');
+    mostrarMasContainer.classList.remove('active');
 });
 
-// Obtener elementos del DOM
-var modal = document.getElementById('modal');
-var modalRevisarAlerta = document.getElementById('modal-revisar-alerta');
-var btnAbrirModal = document.querySelector('.vincular-dispositivo button');
-var btnAbrirModalRevisarAlerta = document.querySelector('.revisar-alerta button');
-var btnCerrarModal = modal.querySelector('.close');
-var btnCerrarModalRevisarAlerta = modalRevisarAlerta.querySelector('.close');
-var btnIncorrecto = document.getElementById('incorrecto');
+const modal = document.getElementById('modal');
+const modalRevisarAlerta = document.getElementById('modal-revisar-alerta');
+const btnAbrirModal = document.querySelector('.vincular-dispositivo button');
+const btnAbrirModalRevisarAlerta = document.querySelector('.revisar-alerta button');
+const btnCerrarModal = modal.querySelector('.close');
+const btnCerrarModalRevisarAlerta = modalRevisarAlerta.querySelector('.close');
+const btnIncorrecto = document.getElementById('incorrecto');
 
-// Función para abrir el primer modal
 btnAbrirModal.onclick = function() {
-  modal.style.display = 'block';
-}
+    modal.style.display = 'block';
+};
 
-// Función para cerrar el primer modal
 btnCerrarModal.onclick = function() {
-  modal.style.display = 'none';
-}
-
-// Función para abrir el segundo modal
-btnAbrirModalRevisarAlerta.onclick = function() {
-  modalRevisarAlerta.style.display = 'block';
-}
-
-// Función para cerrar el segundo modal
-btnCerrarModalRevisarAlerta.onclick = function() {
-  modalRevisarAlerta.style.display = 'none';
-}
-
-// Cerrar el segundo modal al hacer clic en el botón "x" rojo
-btnIncorrecto.onclick = function() {
-  modalRevisarAlerta.style.display = 'none';
-}
-
-// Cerrar los modales cuando se hace clic fuera de ellos
-window.onclick = function(event) {
-  if (event.target == modal) {
     modal.style.display = 'none';
-  } else if (event.target == modalRevisarAlerta) {
+};
+
+btnAbrirModalRevisarAlerta.onclick = function() {
+    modalRevisarAlerta.style.display = 'block';
+};
+
+btnCerrarModalRevisarAlerta.onclick = function() {
     modalRevisarAlerta.style.display = 'none';
-  }
-}
+};
+
+btnIncorrecto.onclick = function() {
+    modalRevisarAlerta.style.display = 'none';
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    } else if (event.target == modalRevisarAlerta) {
+        modalRevisarAlerta.style.display = 'none';
+    }
+};
